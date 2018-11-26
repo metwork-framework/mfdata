bb5ef5aa6c5399852703c431a6662c886d8a5abf
thebaptiste <jean-baptiste.veslin@meteo.fr>
2018-11-26 13:21:17 +0100

    build: remove old stuff

--- integration_tests/convert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-original_basename=$(print_tags $1 |grep "original_basename" |cut -d "'" -f 2 |cut -d "." -f 1)
-convert "$1" "$(dirname $1)/$original_basename.jpeg"
-inject_file --plugin=test_move "$(dirname $1)/$original_basename.jpeg"
