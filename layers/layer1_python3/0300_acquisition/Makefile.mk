404b1ef7e624ee8e165f22bf2f44c562454a1e60
Fabien MARTY <fabien.marty@gmail.com>
2019-03-04 11:38:41 +0100

    build: improve makefiles

--- layers/layer1_python3/0300_acquisition/Makefile.mk
+++ layers/layer1_python3/0300_acquisition/Makefile.mk
@@ -5,19 +5,16 @@ EGG=acquisition-0.0.0-py$(PYTHON_SHORT_VERSION).egg
 
 clean:: pythonclean
 
-all:: $(PREFIX)/opt/python$(METWORK_PYTHON_MODE)/lib/python$(PYTHON_SHORT_VERSION)/site-packages/$(EGG)/acquisition/__init__.py
+all:: dist/$(EGG)
 
-$(PREFIX)/opt/python$(METWORK_PYTHON_MODE)/lib/python$(PYTHON_SHORT_VERSION)/site-packages/$(EGG)/acquisition/__init__.py:
+dist/$(EGG):
 	python setup.py install --prefix=$(PREFIX)/opt/python$(METWORK_PYTHON_MODE)
 
-test::
+test:
 	@echo "***** PYTHON TESTS *****"
 	flake8.sh --exclude=build .
 	find . -name "*.py" ! -path './build/*' -print0 |xargs -0 pylint.sh --errors-only
-	cd tests && nosetests$(METWORK_PYTHON_MODE).sh .
+	cd tests && nosetests.sh .
 
 coverage::
 	cd tests && nosetests.sh --with-coverage --cover-package=acquisition --cover-erase --cover-html --cover-html-dir=coverage .
-
-plop:
-	echo $(MFDATA_HOME) $(PREFIX)
