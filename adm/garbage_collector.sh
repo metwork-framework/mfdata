f25aa03fd63c2c798b877c0efc280207087d6ad2
Fabien MARTY <fabien.marty@gmail.com>
2019-04-25 11:52:51 +0200

    fix: don't clean nginx upload tmp directory
    Close: #126

--- adm/garbage_collector.sh
+++ adm/garbage_collector.sh
@@ -8,7 +8,7 @@ if test -d ${MFDATA_DATA_IN_DIR}/trash; then
 fi
 if test -d ${MFDATA_DATA_IN_DIR}/tmp; then
     find ${MFDATA_DATA_IN_DIR}/tmp -type f -mmin +${MFDATA_DATA_IN_TMP_MAX_LIFETIME} -exec rm -fv {} \; 2>/dev/null
-    find ${MFDATA_DATA_IN_DIR}/tmp -mindepth 1 -type d -exec rmdir {} \; 2>/dev/null
+    find ${MFDATA_DATA_IN_DIR}/tmp -mindepth 1 -type d ! -path "${MFDATA_DATA_IN_DIR}/tmp/nginx" -exec rmdir {} \; 2>/dev/null
 fi
 find ${MFDATA_DATA_IN_DIR} -type f -not -path "${MFDATA_DATA_IN_DIR}/trash/*" -not -path "${MFDATA_DATA_IN_DIR}/tmp/*" -mmin +${MFDATA_DATA_IN_OTHER_MAX_LIFETIME} -exec rm -fv {} \; 2>/dev/null
 
