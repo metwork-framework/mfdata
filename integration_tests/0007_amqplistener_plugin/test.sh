75bf6ea25b60b5d1c75bcd1b066e13a23c81988d
Jean-Baptiste VESLIN <19856429+thebaptiste@users.noreply.github.com>
2024-09-17 17:31:41 +0200

    build: remove warning messages in integration tests (#501)

--- integration_tests/0007_amqplistener_plugin/test.sh
+++ integration_tests/0007_amqplistener_plugin/test.sh
@@ -6,6 +6,7 @@ source ../support.sh
 check_no_tags_left_in_redis
 
 plugins.uninstall --clean foobar5 >/dev/null 2>&1
+rm -f "${MFMODULE_RUNTIME_HOME}/config/plugins/foobar5.ini"
 
 plugins.install --new-name=foobar5 ${MFDATA_HOME}/share/plugins/amqplistener-*.plugin
 
--- integration_tests/0008_mqttlistener_plugin/test.sh
+++ integration_tests/0008_mqttlistener_plugin/test.sh
@@ -6,6 +6,7 @@ source ../support.sh
 check_no_tags_left_in_redis
 
 plugins.uninstall --clean foobar6 >/dev/null 2>&1
+rm -f "${MFMODULE_RUNTIME_HOME}/config/plugins/foobar6.ini"
 
 plugins.install --new-name=foobar6 ${MFDATA_HOME}/share/plugins/mqttlistener-*.plugin
 
